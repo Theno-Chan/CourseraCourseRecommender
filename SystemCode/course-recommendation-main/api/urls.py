7817d53eeaf6622d89a72562316e17f805da4e5e
theno <thenochan123@gmail.com>
2022-10-29 21:03:26 +0800

    second version

---
+++ SystemCode/course-recommendation-main/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, re_path
+from api import views
+
+urlpatterns = [
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('logout/', views.logout, name='logout'),
+    path('profile/', views.profile, name='profile'),
+    path('recommendation/', views.recommendation, name='recommendation'),
+    path('course/<int:course_id>', views.course, name='course'),
+]
